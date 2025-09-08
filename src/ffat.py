bc1e37901ff99636c62aa0f99323b47d29261f85
Harshit Sethi <hstsethi@outlook.com>
2025-09-08 06:20:32 +0530

    Avoid conversion to list

--- src/ffat.py
+++ src/ffat.py
@@ -1,5 +1,4 @@
 import yfinance as yf
-from statistics import median
 from enum import Enum
 
 class Metrics(Enum):
@@ -12,7 +11,7 @@ class Metrics(Enum):
     fpe = "forwardPE"
 
 def getFundEquityHoldings(fund_id: str):
-    return list(yf.Ticker(fund_id).funds_data.top_holdings.index)
+    return yf.Ticker(fund_id).funds_data.top_holdings.index
 
 def getMetric(ticker, metric):
     return yf.Ticker(ticker).info.get(metric)
@@ -22,9 +21,9 @@ def main():
     METRIC = Metrics.pe.value # Add any value from Metrics enum
     fund_holdings = getFundEquityHoldings(FUND_ID)
     t_metrics = [getMetric(h, METRIC) for h in fund_holdings]
-    t_metrics = [0 if x is None else x for x in t_metrics] # Replace None with Zero
+   # t_metrics = [0 if x is None else x for x in t_metrics] # Replace None with Zero
     print("holdings: ", fund_holdings)
-    print(f"median{METRIC}: {median(t_metrics)}")
+    print(f"median{METRIC}:", t_metrics.median())
 
 
 if __name__ == "__main__":
